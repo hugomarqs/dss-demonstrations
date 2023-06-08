044a724683fdeac72411ffda2160dbae270d8976
Hugo Marques <hugomaarqz@gmail.com>
2023-06-08 20:58:02 +0100

    dss 5.11.x

--- sscd-mocca-adapter/src/main/java/eu/europa/esig/dss/token/mocca/MOCCASignatureTokenConnection.java
+++ sscd-mocca-adapter/src/main/java/eu/europa/esig/dss/token/mocca/MOCCASignatureTokenConnection.java
@@ -1,26 +1,26 @@
 /**
  * DSS - Digital Signature Services
  * Copyright (C) 2015 European Commission, provided under the CEF programme
- * 
+ *
  * This file is part of the "DSS - Digital Signature Services" project.
- * 
+ *
  * Licensed under the EUPL, Version 1.1 or – as soon they
  * will be approved by the European Commission - subsequent
  * versions of the EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the
  * Licence.
- * 
+ *
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
+ *
  * Unless required by applicable law or agreed to in
  * writing, software distributed under the Licence is
  * distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied.
- * 
+ *
  * See the Licence for the specific language governing
  * permissions and limitations under the Licence.
  */
@@ -224,7 +224,7 @@ public class MOCCASignatureTokenConnection implements SignatureTokenConnection {
 	public SignatureValue signDigest(Digest digest, DSSPrivateKeyEntry keyEntry) throws DSSException {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	@Override
 	public SignatureValue signDigest(Digest digest, MaskGenerationFunction mgf, DSSPrivateKeyEntry keyEntry)
 			throws DSSException {
